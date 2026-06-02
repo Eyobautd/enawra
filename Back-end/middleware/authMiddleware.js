bc11a9de957e8c21100eb33c6886e501cbcb4dd7
Mym776 <Mymhs7@gmail.com>
2026-06-02 10:48:31 +0300

    update middleware

--- Back-end/middleware/authMiddleware.js
+++ Back-end/middleware/authMiddleware.js
@@ -4,11 +4,11 @@ const protect = (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try{
+        try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = { id: decoded.id };
-            next();
+            return next();
         } catch (error) {
             return res.status(401).json({ message: 'Not authorized, token failed' });
         }
@@ -17,6 +17,6 @@ const protect = (req, res, next) => {
     if (!token) {
         return res.status(401).json({ message: 'Not authorized, no token' });
     }
-}
+};
 
 module.exports = { protect };
